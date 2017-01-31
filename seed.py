d0ac73b4615aa504c2938938b5a2ea1b758822a6
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-30 17:45:19 -0800

    Add data loading functions for ratings and movies to seed.py

--- seed.py
+++ seed.py
@@ -2,8 +2,10 @@
 
 from sqlalchemy import func
 from model import User
-# from model import Rating
-# from model import Movie
+from model import Rating
+from model import Movie
+
+from datetime import datetime
 
 from model import connect_to_db, db
 from server import app
@@ -37,9 +39,56 @@ def load_users():
 def load_movies():
     """Load movies from u.item into database."""
 
+    print "Movies"
+
+    # Delete all rows in table, so if we need to run this a second time,
+    # we won't be trying to add duplicate movies
+    Movie.query.delete()
+
+    # Read u.item file and insert data
+    for row in open("seed_data/u.item"):
+        row = row.rstrip().split("|")
+
+        movie = Movie(movie_id=row[0],
+                      title=row[1][:-7],  # Stripping year e.g. _(1995) from title
+                      released_at=row[2],
+                      imdb_url=row[4])
+
+        # If released_at is not an empty string, convert to datetime
+        if movie.released_at:
+            movie.released_at = datetime.strptime(row[2], '%d-%b-%Y')
+        else:
+            movie.released_at = None
+
+        # We need to add to the session or it won't ever be stored
+        db.session.add(movie)
+
+    # Once we're done, we should commit our work
+    db.session.commit()
+
 
 def load_ratings():
     """Load ratings from u.data into database."""
+
+    print "Ratings"
+
+    # Delete all rows in table, so if we need to run this a second time,
+    # we won't be trying to add duplicate ratings
+    Rating.query.delete()
+
+    # Read u.data file and insert data
+    for row in open("seed_data/u.data"):
+        row = row.rstrip().split("\t")
+
+        rating = Rating(user_id=row[0],
+                        movie_id=row[1],
+                        score=row[2])
+
+        # We need to add to the session or it won't ever be stored
+        db.session.add(rating)
+
+    # Once we're done, we should commit our work
+    db.session.commit()
 
 
 def set_val_user_id():

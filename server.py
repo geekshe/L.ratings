2cde35e06377c053410566742da75267cbff1fdf
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-31 18:03:17 -0800

    Add registration and login

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from flask import Flask, render_template, redirect, request, flash, session, jso
 from flask_debugtoolbar import DebugToolbarExtension
 
 from model import User, Rating, Movie, connect_to_db, db
+import sqlalchemy
 
 
 app = Flask(__name__)
@@ -32,6 +33,83 @@ def user_list():
     users = User.query.all()
     return render_template("user_list.html", users=users)
 
+@app.route('/register', methods=["GET"])
+def display_registration():
+    """Display register form"""
+
+    return render_template("register_form.html")
+
+@app.route('/register', methods=["POST"])
+def process_registration():
+    """Process a new user from registration form"""
+
+    # Grab inputs from registration form
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    # If user doesn't exist, add them to the database
+    if len(User.query.filter_by(email=username).all()) == 0:
+        user = User(email=username,
+                    password=password)
+
+        # Add user to the session
+        db.session.add(user)
+
+        # Commit transaction to db
+        db.session.commit()
+
+        print "user %s added to db" % (username)
+
+        flash("New account created")
+
+    return redirect("/login")
+
+@app.route('/login', methods=["GET"])
+def display_login():
+    """Display login form"""
+
+    return render_template('login_form.html')
+
+@app.route('/login', methods=["POST"])
+def log_in():
+    """Log user in"""
+
+    # Grab username and password from form
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    # Query for user in database
+    user = User.query.filter_by(email=username)
+
+    # user.all()
+
+    # If user in database and if password matches, login
+    try:
+        login_user = user.one():
+    # Throws an exception if user.one does not return an item from the database
+    except sqlalchemy.orm.exc.NoResultFound:
+        flash("No User with username %s" % (username))
+        return redirect("/login")
+
+    if login_user.password == password:
+        # Add user to session cookie
+        session['username'] = username
+        flash("Logged in")
+        return redirect("/")
+    else:
+        flash("Incorrect password")
+        return redirect("/login")
+
+
+@app.route('/logout')
+def log_out():
+    """Log user out"""
+
+    # Remove user from session
+    del session['username']
+
+    flash("Logged out")
+    return redirect("/")
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
@@ -43,5 +121,7 @@ if __name__ == "__main__":
 
     # Use the DebugToolbar
     DebugToolbarExtension(app)
+
+    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
     app.run(port=5000, host='0.0.0.0')
